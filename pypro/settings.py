94295b06a53dc925bb4cdff1dca15a335b42bfcb
fernandolcouto <fernandolcouto@gmail.com>
2021-03-20 10:38:06 -0300

    Teste: modified:   pypro/settings.py

--- pypro/settings.py
+++ pypro/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+import django-heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pypro',
 ]
 
 MIDDLEWARE = [
